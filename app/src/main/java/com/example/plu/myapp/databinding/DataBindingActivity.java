a2bac6d5a16a24c5b0177c299e2d8e6c98b5cb16
chengXing <ccharp@163.com>
2016-09-28 17:35:11 +0800

    修改了实现

--- app/src/main/java/com/example/plu/myapp/databinding/DataBindingActivity.java
+++ app/src/main/java/com/example/plu/myapp/databinding/DataBindingActivity.java
@@ -3,12 +3,13 @@ package com.example.plu.myapp.databinding;
 import android.os.Bundle;
 
 import com.example.plu.myapp.base.activity.DaggerActivity;
+import com.example.plu.myapp.dagger.base.BaseComponent;
 
 /**
  * Created by ccharp on 16/9/25.
  */
 
-public class DataBindingActivity extends DaggerActivity {
+public class DataBindingActivity extends DaggerActivity<BaseComponent> {
     @Override
     protected void initData(Bundle state) {
 
