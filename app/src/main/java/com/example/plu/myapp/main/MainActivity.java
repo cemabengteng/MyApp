d06bac678a582e4fedbd256d0d6197cb1c8fb4ea
chengXing <ccharp@163.com>
2016-09-29 16:29:08 +0800

    增加更加流畅的飘心动画

--- app/src/main/java/com/example/plu/myapp/main/MainActivity.java
+++ app/src/main/java/com/example/plu/myapp/main/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.plu.myapp.main;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.plu.myapp.R;
 import com.example.plu.myapp.base.activity.MvpActivity;
 import com.example.plu.myapp.dagger.component.ActivityComponent;
 import com.example.view.heartview.NewHeartAnimSurfaceView;
+import com.example.view.newheartview.GoodAnimationUtile;
+import com.example.view.newheartview.GoodsInitUtile;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import javax.inject.Inject;
@@ -28,7 +37,11 @@ public class MainActivity extends MvpActivity<MainComponent, MainPresenter> {
     MainPresenter mPresenter;
     @Bind(R.id.heartView)
     NewHeartAnimSurfaceView heartView;
+    @Bind(R.id.thumbContainer)
+    RelativeLayout thumbContainer;
+
     private Random mRandom;
+    private List<ImageView> lsImgGoods = new ArrayList<>();
 
     @Override
     protected MainPresenter createPresenter() {
@@ -45,6 +58,26 @@ public class MainActivity extends MvpActivity<MainComponent, MainPresenter> {
     @Override
     protected void initData(Bundle state) {
         mRandom = new Random();
+        initGoodImageViews();
+    }
+
+    private void initGoodImageViews() {
+        for (int i = 0; i < 25; i++) {
+            ImageView img = new ImageView(this);
+            RelativeLayout.LayoutParams rlPram = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            rlPram.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+            // 水平居中
+            rlPram.addRule(RelativeLayout.CENTER_HORIZONTAL);
+            img.setLayoutParams(rlPram);
+            img.setImageResource(R.drawable.ic_qipao_zi_3);
+            GoodHolder holderTag = new GoodHolder();
+            holderTag.time = System.currentTimeMillis();
+            img.setTag(holderTag);
+            img.setScaleX(0.1f);
+            img.setScaleY(0.1f);
+            lsImgGoods.add(img);
+
+        }
     }
 
     @Override
@@ -53,7 +86,21 @@ public class MainActivity extends MvpActivity<MainComponent, MainPresenter> {
 
     }
 
-    @OnClick(R.id.btStarHeartAni)
+    private final Handler msgReceivHandle = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 1) {
+                if (msg.arg1 == 1) {
+                    setGoodsUiTouch(GoodsInitUtile.getGoodsType(msg.arg2));
+                }
+            }
+        }
+
+    };
+
+    @OnClick({R.id.btStarHeartAni, R.id.btStart})
     public void click(View v) {
         switch (v.getId()) {
             case R.id.btStarHeartAni:
@@ -61,7 +108,110 @@ public class MainActivity extends MvpActivity<MainComponent, MainPresenter> {
 //                heartView.addHeartsNow(mRandom.nextInt(100),5000);
                 heartView.addHeartNow();
                 break;
+            case R.id.btStart:
+                sendThumbs();
+                break;
         }
+    }
+
+    private void sendThumbs() {
+        Message msgMsg = msgReceivHandle.obtainMessage();
+        msgMsg.what = 1;
+        msgMsg.arg1 = 1;//点赞
+        int random = (int) (Math.random() * 100 + 1) % 14;
+        msgMsg.arg2 = random;
+        msgReceivHandle.sendMessage(msgMsg);
+    }
+
+    /**
+     * 点赞的话，开始设置赞的效果
+     */
+    public void setGoodsUiTouch(int resource) {
+        ImageView goodImgView = getGoodImageView();
+        if (goodImgView == null) {
+            return;
+        }
+        ((GoodHolder) goodImgView.getTag()).isAdd = true;
+        ((GoodHolder) goodImgView.getTag()).time = System.currentTimeMillis();
+        goodImgView.setImageResource(resource);
+        thumbContainer.addView(goodImgView);
+        goodImgView.setVisibility(View.INVISIBLE);
+
+        Animation anim = GoodAnimationUtile.createAnimation(this);
+        anim.setAnimationListener(new GoodMsgAnimaionList(goodImgView));
+        goodImgView.startAnimation(anim);
+    }
+
+    private ImageView getGoodImageView() {
+        for (int i = 0; i < lsImgGoods.size(); i++) {
+            ImageView imgtem = lsImgGoods.get(i);
+            GoodHolder tag = (GoodHolder) imgtem.getTag();
+            if (tag.isAdd == false) {
+                return imgtem;
+            }
+        }
+        return null;
+    }
+
+    class GoodHolder {
+        public boolean isAdd = false;
+        public long time;
+    }
+
+    private Handler moveHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            for (int i = 0; i < thumbContainer.getChildCount(); i++) {
+                ImageView imgv = null;
+                try {
+                    imgv = (ImageView) thumbContainer.getChildAt(i);
+                } catch (Exception e) {
+                    imgv = null;
+                }
+                if (imgv == null) {
+                    continue;
+                }
+
+                long time = ((GoodHolder) imgv.getTag()).time;
+                long before = (Long) msg.obj;
+
+                if (time == before) {
+                    ((GoodHolder) imgv.getTag()).isAdd = false;
+                    thumbContainer.removeView(imgv);
+                }
+            }
+        }
+
+    };
+
+    class GoodMsgAnimaionList implements Animation.AnimationListener {
+        private ImageView imgv;
+
+        public GoodMsgAnimaionList(ImageView imgv) {
+            this.imgv = imgv;
+        }
+
+        @Override
+        public void onAnimationEnd(Animation arg0) {
+            imgv.clearAnimation();
+            imgv.setVisibility(View.INVISIBLE);
+
+            Message msg = moveHandler.obtainMessage();
+            msg.what = 1;
+            msg.obj = ((GoodHolder) (imgv.getTag())).time;
+            moveHandler.sendMessage(msg);
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation arg0) {
+        }
+
+        @Override
+        public void onAnimationStart(Animation arg0) {
+        }
+
     }
 
     @Override
---
+++ view/src/main/java/com/example/view/newheartview/GoodsInitUtile.java
@@ -0,0 +1,113 @@
+package com.example.view.newheartview;
+
+
+import com.example.view.R;
+
+public class GoodsInitUtile {
+
+    private static int goodType1 = R.drawable.ic_qipao_fen_1;
+    private static int goodType2 = R.drawable.ic_qipao_fen_2;
+    private static int goodType3 = R.drawable.ic_qipao_fen_3;
+    private static int goodType4 = R.drawable.ic_qipao_hong_1;
+    private static int goodType5 = R.drawable.ic_qipao_hong_2;
+    private static int goodType6 = R.drawable.ic_qipao_hong_3;
+    private static int goodType7 = R.drawable.ic_qipao_huang_1;
+    private static int goodType8 = R.drawable.ic_qipao_huang_2;
+    private static int goodType9 = R.drawable.ic_qipao_huang_3;
+    private static int goodType10 = R.drawable.ic_qipao_ju_1;
+    private static int goodType11 = R.drawable.ic_qipao_ju_2;
+    private static int goodType12 = R.drawable.ic_qipao_ju_3;
+    private static int goodType13 = R.drawable.ic_qipao_lan_1;
+    private static int goodType14 = R.drawable.ic_qipao_lan_2;
+    private static int goodType15 = R.drawable.ic_qipao_lan_3;
+    private static int goodType16 = R.drawable.ic_qipao_lv_1;
+    private static int goodType17 = R.drawable.ic_qipao_lv_2;
+    private static int goodType18 = R.drawable.ic_qipao_lv_3;
+    private static int goodType19 = R.drawable.ic_qipao_zi_1;
+    private static int goodType20 = R.drawable.ic_qipao_zi_2;
+    private static int goodType21 = R.drawable.ic_qipao_zi_3;
+
+    private static int TYPECOUNT = 14;
+
+    /*
+     *   不同用户的标识id
+     */
+    public static int fromGoodID(int _id) {
+        int rint = (int) (_id % TYPECOUNT);
+        return rint;
+    }
+
+    /*
+     * 获取点赞的类型
+     */
+    public static int getGoodsType(int type) {
+        int rType = goodType1;
+        switch (type) {
+            case 1:
+                rType = goodType1;
+                break;
+            case 2:
+                rType = goodType2;
+                break;
+            case 3:
+                rType = goodType3;
+                break;
+            case 4:
+                rType = goodType4;
+                break;
+            case 5:
+                rType = goodType5;
+                break;
+            case 6:
+                rType = goodType6;
+                break;
+            case 7:
+                rType = goodType7;
+                break;
+            case 8:
+                rType = goodType8;
+                break;
+            case 9:
+                rType = goodType9;
+                break;
+            case 10:
+                rType = goodType10;
+                break;
+            case 11:
+                rType = goodType11;
+                break;
+            case 12:
+                rType = goodType12;
+                break;
+            case 13:
+                rType = goodType13;
+                break;
+            case 14:
+                rType = goodType14;
+                break;
+            case 15:
+                rType = goodType15;
+                break;
+            case 16:
+                rType = goodType16;
+                break;
+            case 17:
+                rType = goodType17;
+                break;
+            case 18:
+                rType = goodType18;
+                break;
+            case 19:
+                rType = goodType19;
+                break;
+            case 20:
+                rType = goodType20;
+                break;
+            case 21:
+                rType = goodType21;
+                break;
+        }
+        return rType;
+    }
+
+}
