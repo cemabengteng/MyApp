7dce8906bbf13d2c32677b193e6f589257dd8c70
chengXing <ccharp@163.com>
2016-10-12 17:55:21 +0800

    添加了zip包下载解压代码

---
+++ app/src/main/java/com/example/plu/myapp/downloadgiftzip/DownLoadGiftTextActivity.java
@@ -0,0 +1,26 @@
+package com.example.plu.myapp.downloadgiftzip;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.plu.myapp.R;
+
+/**
+ * Created by chengXing on 2016/10/12.
+ */
+
+public class DownLoadGiftTextActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download_gift_test);
+        gogogo();
+    }
+
+    private void gogogo() {
+        DownloadGiftZip downloadGiftZip = new DownloadGiftZip("0xb9ffc158",
+                "http://img.plures.net/v2/303f/6124/f5e6/60b7/101f/3a84/b225/f895.zip",
+                this);
+
+    }
+}
--- app/src/main/java/com/example/plu/myapp/downloadgiftzip/DownloadGiftZip.java
+++ app/src/main/java/com/example/plu/myapp/downloadgiftzip/DownloadGiftZip.java
@@ -5,8 +5,17 @@ import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.CRC32;
+import java.util.zip.CheckedInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  * Created by chengXing on 2016/10/12.
@@ -35,17 +44,115 @@ public class DownloadGiftZip {
         } else {
             if (isHaveGiftZip()) {
                 Log.i("test", "存在gift压缩包");
+                //TODO 校验压缩包  解压
+                if (verifyZip()) {
+                    try {
+                        unCompressZip();
+                        Log.i("test", "解压完成");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    //删除当前zip，从新下载
+                    downLoadZip();
+                }
+
             } else {
                 //下载zip包
-                File file = new File(mStorageDirectory.getAbsolutePath(), "0xb9ffc158.zip");
+                downLoadZip();
+            }
+        }
+    }
+
+    private void downLoadZip() {
+        File file = new File(mStorageDirectory.getAbsolutePath(), mValueOfCrc32 + ".zip");
+        try {
+            file.createNewFile();
+            Log.i("test", "下载了zip文件");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean verifyZip() {
+        File file = new File(mStorageDirectory.getAbsolutePath(), mValueOfCrc32 + ".zip");
+        try {
+            CheckedInputStream cis = new CheckedInputStream(
+                    new FileInputStream(file), new CRC32());
+            byte[] buf = new byte[128];
+            while (cis.read(buf) >= 0) {
+            }
+            long checksum = cis.getChecksum().getValue();
+            String crc32 = Long.toHexString(checksum);
+            Log.i("test", crc32);
+            if (("0x" + crc32).equals(mValueOfCrc32)) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private void unCompressZip() throws Exception {
+        File file = new File(mStorageDirectory.getAbsolutePath(), mValueOfCrc32 + ".zip");
+        ZipFile zipFile = new ZipFile(file);
+        try {
+            Enumeration<?> entrys = zipFile.entries();
+            while (entrys.hasMoreElements()) {
+                ZipEntry zipEntry = (ZipEntry) entrys.nextElement();
+                if (zipEntry.isDirectory()) {
+                    File temp = new File(mStorageDirectory.getAbsolutePath() + zipEntry.getName());
+                    if (!temp.exists()) {
+                        if (!temp.mkdirs()) {
+                            throw new Exception();
+                        }
+                    }
+                } else {
+                    File f = new File(mStorageDirectory.getAbsolutePath() + "/" + mValueOfCrc32 + "/");
+                    if (!f.exists()){
+                        f.mkdir();
+                    }
+                    BufferedInputStream input = new BufferedInputStream(
+                            zipFile.getInputStream(zipEntry));
+                    BufferedOutputStream output = new BufferedOutputStream(
+                            new FileOutputStream(mStorageDirectory.getAbsolutePath() + "/" + mValueOfCrc32 + "/" + zipEntry.getName()));
+                    Log.i("test","path: " + mStorageDirectory.getAbsolutePath() + "/" + mValueOfCrc32 + "/" + zipEntry.getName());
+
+                    int len = -1;
+                    byte[] bytes = new byte[2048];
+                    while ((len = input.read(bytes)) != -1) {
+                        output.write(bytes, 0, len);
+                    }
+                    output.close();
+                    input.close();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != zipFile) {
                 try {
-                    file.createNewFile();
-                    Log.i("test", "下载了zip文件");
+                    zipFile.close();
+                    zipFile = null;
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
+
+
+//        ZipInputStream zis = new ZipInputStream(new FileInputStream(file));
+//        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(mStorageDirectory.getAbsolutePath() + "/" +  mValueOfCrc32));
+//        int count;
+//        byte data[] = new byte[1024];
+//        while ((count = zis.read(data, 0, 1024)) != -1) {
+//            bos.write(data, 0, count);
+//        }
+//        zis.close();
+//        bos.close();
     }
 
     private boolean isHavaGiftFile() {
