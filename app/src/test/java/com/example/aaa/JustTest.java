6e3a75051f5f678db6f2ad966dc08e1f3fc27d0d
chengXing <ccharp@163.com>
2016-10-26 15:52:14 +0800

    开始琢磨mockito测试框架

---
+++ app/src/test/java/com/example/aaa/JustTest.java
@@ -0,0 +1,7 @@
+package com.example.aaa;
+
+/**
+ * Created by chengXing on 2016/10/26.
+ */
+public class JustTest {
+}
---
+++ app/src/test/java/com/example/aaa/PresenterTest.java
@@ -0,0 +1,9 @@
+package com.example.aaa;
+
+/**
+ * Created by chengXing on 2016/10/26.
+ */
+
+public class PresenterTest {
+
+}
