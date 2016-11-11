5d6cfc666f3d03d73c13d8ad0e8cbd39707f44c6
chengXing <ccharp@163.com>
2016-11-11 18:20:06 +0800

    增加Fragment操作方法

--- app/src/main/java/com/example/plu/myapp/base/fragment/BaseFragment.java
+++ app/src/main/java/com/example/plu/myapp/base/fragment/BaseFragment.java
@@ -2,12 +2,17 @@ package com.example.plu.myapp.base.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.plu.myapp.base.rx.RxFragment;
+
+import org.greenrobot.eventbus.EventBus;
 
 import butterknife.ButterKnife;
 
@@ -22,9 +27,14 @@ public abstract class BaseFragment extends RxFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mContext = getActivity();
         View v = getInflateView();
-        ButterKnife.bind(v);
+        if (v == null) {
+            v = View.inflate(mContext, getLayout(), null);
+        }
+        ButterKnife.bind(this, v);
+        EventBus.getDefault().register(this);
         return v;
     }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -38,6 +48,32 @@ public abstract class BaseFragment extends RxFragment {
     public void onDestroy() {
         super.onDestroy();
         ButterKnife.unbind(this);
+        EventBus.getDefault().unregister(this);
+    }
+
+    protected <T extends Fragment> T findFragment(Class<T> fragmentClazz, String tag, Bundle bundle) {
+        FragmentManager fragmentManager = getChildFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        if (fragment == null) {
+            fragment = Fragment.instantiate(getActivity(), fragmentClazz.getName(), bundle);
+        }
+        return (T) fragment;
+    }
+
+    protected void hideFragment(String tag) {
+        FragmentManager fragmentManager = getChildFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        if (fragment != null) {
+            fragmentManager.beginTransaction().hide(fragment).commitAllowingStateLoss();
+        }
+    }
+
+    protected void removeFragment(String tag) {
+        FragmentManager fragmentManager = getChildFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        if (fragment != null) {
+            fragmentManager.beginTransaction().remove(fragment).commitAllowingStateLoss();
+        }
     }
 
     protected abstract void initListener();
@@ -45,5 +81,8 @@ public abstract class BaseFragment extends RxFragment {
     protected abstract void initData();
 
     public abstract View getInflateView();
+
+    @LayoutRes
+    protected abstract int getLayout();
 
 }
